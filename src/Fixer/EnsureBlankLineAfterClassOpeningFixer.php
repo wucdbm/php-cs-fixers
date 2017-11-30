6595e2a0d000366705a01842217a32bc467abb84
Martin Kirilov <wucdbm@gmail.com>
2017-11-30 18:43:13 +0200

    cover the case where there's no white space at all following class declaration, try to figure out class declaration indentation and add another one for members to follow

--- src/Fixer/EnsureBlankLineAfterClassOpeningFixer.php
+++ src/Fixer/EnsureBlankLineAfterClassOpeningFixer.php
@@ -8,7 +8,6 @@ use PhpCsFixer\FixerDefinition\CodeSample;
 use PhpCsFixer\FixerDefinition\FixerDefinition;
 use PhpCsFixer\Tokenizer\Token;
 use PhpCsFixer\Tokenizer\Tokens;
-use PhpCsFixer\Utils;
 
 /**
  * @author Ceeram <ceeram@cakephp.org>
@@ -52,25 +51,28 @@ final class Sample {
             }
 
             $startBraceIndex = $tokens->getNextTokenOfKind($index, ['{']);
-            if (!$tokens[$startBraceIndex + 1]->isWhitespace()) {
-                continue;
-            }
 
-            $this->fixWhitespace($tokens, $startBraceIndex + 1);
+            $whitespace = $this->createBlankLine($tokens, $startBraceIndex);
+            $nextTokenIndex = $startBraceIndex + 1;
+
+            if (!$tokens[$nextTokenIndex]->isWhitespace()) {
+                // If there is no white space at all, we need two lines and a single indentation
+                // But in case the declaration is wrapped in an if statement,
+                // Then we need the current indentation plus one additional
+                $tokens->insertAt($nextTokenIndex, new Token([T_WHITESPACE, $whitespace]));
+            } else {
+                // If there is any white space, then we can just replace it
+                // What we need is two line endings, the class declaration indentation
+                // And another indent for any following token (property, constant, method, etc)
+                $tokens[$nextTokenIndex] = new Token([T_WHITESPACE, $whitespace]);
+            }
         }
     }
 
-    /**
-     * Replace any white space with a single blank line.
-     *
-     * @param Tokens $tokens
-     * @param int    $index
-     */
-    private function fixWhitespace(Tokens $tokens, $index) {
-        $content = $tokens[$index]->getContent();
-        $lines = Utils::splitLines($content);
-        // the final bit of the whitespace must be the next statement's indentation
-        $tokens[$index] = new Token([T_WHITESPACE, $this->whitespacesConfig->getLineEnding() . $this->whitespacesConfig->getLineEnding() . end($lines)]);
+    private function createBlankLine(Tokens $tokens, $startBraceIndex) {
+        $classDeclarationIndent = $this->detectIndent($tokens, $startBraceIndex);
+
+        return $this->whitespacesConfig->getLineEnding() . $this->whitespacesConfig->getLineEnding() . $classDeclarationIndent . $this->whitespacesConfig->getIndent();
     }
 
     public function getPriority() {
@@ -80,5 +82,45 @@ final class Sample {
 
     public function getName() {
         return 'Wucdbm/' . parent::getName();
+    }
+
+    /**
+     * This has been blatantly stolen from BracesFixer and does an excellent job
+     *
+     * @param Tokens $tokens
+     * @param int    $index
+     *
+     * @return string
+     */
+    private function detectIndent(Tokens $tokens, $index) {
+        while (true) {
+            $whitespaceIndex = $tokens->getPrevTokenOfKind($index, [[T_WHITESPACE]]);
+
+            if (null === $whitespaceIndex) {
+                return '';
+            }
+
+            $whitespaceToken = $tokens[$whitespaceIndex];
+
+            if (false !== strpos($whitespaceToken->getContent(), "\n")) {
+                break;
+            }
+
+            $prevToken = $tokens[$whitespaceIndex - 1];
+
+            if ($prevToken->isGivenKind([T_OPEN_TAG, T_COMMENT]) && "\n" === substr($prevToken->getContent(), -1)) {
+                break;
+            }
+
+            $index = $whitespaceIndex;
+        }
+
+        if (!isset($whitespaceToken)) {
+            return '';
+        }
+
+        $explodedContent = explode("\n", $whitespaceToken->getContent());
+
+        return end($explodedContent);
     }
 }
